3d11ed82053ce0372a2a59ba0afe50c39f1a2a54
Priyesh Chaurasia <39457163+priyeshchaurasia@users.noreply.github.com>
2020-10-10 23:38:54 +0530

    Create targetsumsubset.cpp

---
+++ targetsumsubset.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+bool helper(int arr[],int sum,int n){
+    bool t[n+1][sum+1];
+    for(int i=0;i<=n;i++){
+        t[i][0]=true;
+    }
+    for(int i=1;i<=sum;i++){
+        t[0][i]=false;
+    }
+    
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=sum;j++){
+            if(arr[i-1]<=j){
+                t[i][j] = t[i-1][j-arr[i-1]] || t[i-1][j];
+            }
+            else{
+                t[i][j] = t[i-1][j];
+            }
+        }
+    }
+    return t[n][sum];
+    
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+        
+    }
+    int sum=0;
+    cin>>sum;
+    if(helper(arr,sum,n)){
+        cout<<"true";
+    }
+    else{
+    cout<<"false";
+    }
+    return 0;
+        
+    
+}
